9c1f7ee595c3e28115e1e8fe43cf0d2a5a19f323
Xavier Roche <roche+git@exalead.com>
2010-05-10 10:32:03 +0200

    Deleted internal file.

--- conf.h
+++
@@ -1,8 +0,0 @@
-/* must be the last included file */
-#include "text/flow/fastlz/fastlzlib.module.h"
-
-/* exports and inlining */
-#define _NG_NO_ASSERT
-#include "nglib/quirks.h"
-#define ZFASTEXTERN NGPUBLIC
-#define ZFASTINLINE NGFORCEINLINE
